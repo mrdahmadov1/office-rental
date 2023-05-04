828ef1153da6df9c2c340937fd7db6946d56321b
Murad <mur1d1xmedov@gmail.com>
2023-05-04 14:03:34 +0400

    changed interface which in node modules about fire

--- typing.d.ts
+++ typing.d.ts
@@ -1,6 +1,22 @@
+import firebase from 'firebase/compat/app';
+
 declare module '@angular/fire/compat/firestore/interfaces' {
   export interface DocumentSnapshotExists<T>
-    extends firebase.firestore.DocumentSnapshot {
-    data(options?: firebase.firestore.SnapshotOptions): T;
+    extends firebase.firestore.DocumentSnapshot<T> {
+    data(options?: SnapshotOptions): T;
+  }
+
+  export interface QueryDocumentSnapshot<T>
+    extends firebase.firestore.QueryDocumentSnapshot<T> {
+    data(options?: SnapshotOptions): T;
+  }
+
+  export interface QuerySnapshot<T>
+    extends firebase.firestore.QuerySnapshot<T> {
+    readonly docs: QueryDocumentSnapshot<T>[];
+  }
+  export interface DocumentChange<T>
+    extends firebase.firestore.DocumentChange<T> {
+    readonly doc: QueryDocumentSnapshot<T>;
   }
 }

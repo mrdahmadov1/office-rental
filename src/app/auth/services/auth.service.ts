c6041fa45f4c4d1facdc6fdffeaf10e8981a14ef
Murad <mur1d1xmedov@gmail.com>
2023-05-05 16:47:34 +0400

    responsive design was prepared

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -8,6 +8,7 @@ import {
 import firebase from 'firebase/compat/app';
 import { Router } from '@angular/router';
 import { FirebaseError } from 'firebase/app';
+import * as alertifyjs from 'alertifyjs';
 
 @Injectable({
   providedIn: 'root',
@@ -40,14 +41,20 @@ export class AuthService {
       );
       if (result.user) {
         this.router.navigate(['/main/home']);
+        alertifyjs.success('Successful logged in');
       }
     } catch (error) {
       if (error instanceof FirebaseError) {
         if (error.code === 'auth/invalid-email') {
+          alertifyjs.error('Invalid email');
         } else if (error.code === 'auth/user-not-found') {
+          alertifyjs.error('User not found');
         } else if (error.code === 'auth/user-disabled') {
+          alertifyjs.error('User disabled');
         } else if (error.code === 'auth/wrong-password') {
+          alertifyjs.error('Wrong password');
         } else {
+          alertifyjs.error(`${error.message}`);
         }
       }
     }
@@ -61,13 +68,19 @@ export class AuthService {
       );
       await this.sendVerificationMail();
       await this.updateUserData(result.user!);
+      alertifyjs.success('Successful registration');
     } catch (error) {
       if (error instanceof FirebaseError) {
         if (error.code === 'auth/invalid-email') {
+          alertifyjs.error('Invalid email');
         } else if (error.code === 'auth/email-already-in-use') {
+          alertifyjs.error('Email already in use');
         } else if (error.code === 'auth/operation-not-allowed') {
+          alertifyjs.error('Operation not allowed');
         } else if (error.code === 'auth/weak-password') {
+          alertifyjs.error('Wrong password');
         } else {
+          alertifyjs.error(`${error.message}`);
         }
       }
     }
@@ -78,7 +91,9 @@ export class AuthService {
       await (await this.afAuth.currentUser)?.sendEmailVerification();
       this.router.navigate(['/auth/login']);
     } catch (error) {
-      console.log(error);
+      if (error instanceof FirebaseError) {
+        alertifyjs.error(`${error.message}`);
+      }
     }
   }
 
@@ -87,11 +102,15 @@ export class AuthService {
       await this.afAuth.sendPasswordResetEmail(passwordResetEmail);
 
       this.router.navigate(['/auth/login']);
+      alertifyjs.warning('Email has been sent');
     } catch (error) {
       if (error instanceof FirebaseError) {
         if (error.code === 'auth/invalid-email') {
+          alertifyjs.error('Invalid email');
         } else if (error.code === 'auth/user-not-found') {
+          alertifyjs.error('User not found');
         } else {
+          alertifyjs.error(`${error.message}`);
         }
       }
     }
@@ -102,22 +121,25 @@ export class AuthService {
     return user !== null && user.emailVerified !== false;
   }
 
-  async GoogleAuth() {
-    try {
-      const result = await this.afAuth.signInWithPopup(
-        new firebase.auth.GoogleAuthProvider()
-      );
-      this.router.navigate(['dashboard']);
-      await this.updateUserData(result.user!);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+  // async GoogleAuth() {
+  //   try {
+  //     const result = await this.afAuth.signInWithPopup(
+  //       new firebase.auth.GoogleAuthProvider()
+  //     );
+  //     this.router.navigate(['/main/home']);
+  //     await this.updateUserData(result.user!);
+  //   } catch (error) {
+  //     if (error instanceof FirebaseError) {
+  //       alertifyjs.error(`${error.message}`);
+  //     }
+  //   }
+  // }
 
   async SignOut() {
     await this.afAuth.signOut();
     localStorage.removeItem('user');
-    this.router.navigate(['login']);
+    this.router.navigate(['/auth/login']);
+    alertifyjs.warning('Logged out');
   }
 
   private async updateUserData(user: firebase.User) {
